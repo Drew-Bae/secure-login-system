12cef7feca4da15a4c0fd18c26771adf91075a58
Drew Bae <drewbae912@gmail.com>
2025-12-27 13:55:14 -0600

    feat(backend): add riskScore to LoginAttempt

--- backend/src/models/LoginAttempt.js
+++ backend/src/models/LoginAttempt.js
@@ -8,9 +8,10 @@ const loginAttemptSchema = new mongoose.Schema(
     deviceId: { type: String },
     success: { type: Boolean, required: true },
 
-    // New fields
     suspicious: { type: Boolean, default: false },
     reasons: [{ type: String }],
+    
+    riskScore: { type: Number, default: 0 },
   },
   { timestamps: true }
 );
